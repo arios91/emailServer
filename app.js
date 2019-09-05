d621503ee2fd22c39fd39c65921e95f0e1c5455b
Alejandro Rios <Alx.Rios91@gmail.com>
2019-09-05 00:14:39 -0500

    added payment processing path

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+const keys = require('./config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const keys = require('./config/keys');
 const express = require('express');
 const cors = require('cors');
 const port = 8080;
@@ -40,22 +41,57 @@ let transporter = nodemailer.createTransport({
 app.post('/sendEmail', (req, res) =>{
     // setup email data with unicode symbols
     let mailOptions = {
-        from: req.body.originEmail, // sender address
-        to: recipientEmail, // list of receivers
+        from: req.body.customerEmail, // sender address
+        to: req.body.recipientEmail, // list of receivers
         subject: req.body.subject, // Subject line
         text: req.body.textBody, // plain text body
         html: req.body.emailBody // html body
     };
 
-    // send mail with defined transport object
+    sendEmail(mailOptions);
+    res.end();
+});
+
+
+//charge route
+app.post('/charge', (req, res) =>{
+    console.log('received request')
+    //create and charge the customer
+    stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+    })
+    .then(customer => stripe.charges.create({
+        amount: req.body.amount,
+        description: req.body.description,
+        currency: 'usd',
+        customer: customer.id,
+        receipt_email: req.body.email
+    }))
+    .then(charge => {
+    // setup email data with unicode symbols
+        let mailOptions = {
+            from: keys.orderInSender, // sender address
+            to: keys.orderInEmail, // list of receivers
+            subject: 'Order In', // Subject line
+            text: req.body.emailText, // plain text body
+            html: req.body.emailBody // html body
+        };
+        sendEmail(mailOptions)
+        res.end();
+    })
+    .catch((err) => {
+        console.log('error: ' + err);
+    });
+});
+
+const sendEmail = (mailOptions) => {
     transporter.sendMail(mailOptions, error => {
         if (error) {
             return console.log(error);
         }
     });
-    res.end();
-});
-
+}
 
 app.listen(port, () => {
 });
