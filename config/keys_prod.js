3e5ee938e55ba2fde1b6d92ede9d239c603947ad
Alejandro Rios <Alx.Rios91@gmail.com>
2019-09-10 18:48:41 -0500

    test get for heroku

---
+++ config/keys_prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    nodeMailerUser: 'Alx.Rios91@gmail.com',
+    nodeMailerPass: 'Rz1bBIgxVqaZdcC8',
+    whiteList: ['https://www.petalosarte.com', 'http://petalosarte.com'],
+    orderInEmail: 'petalosArteOrders@gmail.com',
+    orderInSender: '"Petalos Arte Flower Shop" <orders@petalosarte.com>'
+}
