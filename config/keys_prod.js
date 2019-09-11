56f4aa9f022ce653d80f9a568f226e95b3d86946
Alejandro Rios <Alx.Rios91@gmail.com>
2019-09-10 19:49:18 -0500

    key test

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,8 @@
 module.exports = {
-    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    // stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    // stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    stripePublishableKey: 'pk_test_Mqk5tVgm8NXvt81KUk3iigKo',
+    stripeSecretKey: 'sk_test_6eY7Gej779d3mEC51fPHukwQ',
     nodeMailerUser: process.env.NODE_MAILER_USER,
     nodeMailerPass: process.env.NODE_MAILER_PASS,
     whiteList: ['https://www.petalosarte.com', 'http://petalosarte.com'],
